894bbadaada645746ac27e6424a13404d5e3ad2d
ZizziWang <cc.wang.nctu@gmail.com>
2015-09-28 04:23:18 +0900

    all MEAS write at once
    uint32_t aCap[2]-uint16_t aCap[8]
    /*all MEAS write at once*/
    const-#define
    pipe-PIPE

--- R_09272015/R_09272015.ino
+++ R_09272015/R_09272015.ino
@@ -5,27 +5,25 @@
 /*-----( Declare Constants and Pin Numbers )-----*/
 #define CE_PIN   9
 #define CSN_PIN 10
-
+#define PIPE 0xE8E8F0F0E1LL
 // NOTE: the "LL" at the end of the constant is "LongLong" type
-const uint64_t pipe = 0xE8E8F0F0E1LL; // Define the transmit pipe
+/*const uint64_t pipe = 0xE8E8F0F0E1LL; // Define the transmit pipe*/
 
 /*-----( Declare objects )-----*/
 RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
 /*-----( Declare Variables )-----*/
-uint32_t aCap[2];
+uint32_t aCap[8];
 uint32_t u32Cap;
 int32_t s32Cap;
-uint32_t u32Cap1;
-uint32_t u32Cap2;
 double fCap;
 
 void setup() {
   Serial.begin(9600);
   delay(1000);
-  Serial.println("Nrf24L01 Receiver Starting");
+  Serial.println("nRF24L01 Receiver Starting");
   radio.begin();
-  radio.openReadingPipe(1,pipe);
-  radio.startListening();;  
+  radio.openReadingPipe(1,PIPE);
+  radio.startListening();
 }//--(end setup )---
 
 void loop() 
@@ -38,20 +36,21 @@ void loop()
     {
       // Fetch the data payload
       radio.read( aCap, sizeof(aCap) );
-      u32Cap1 = aCap[1];
-      u32Cap2 = aCap[2];
-      if((aCap[1]>>15)==0)                      //check capacitance POS/NEG
+      uint32_t u32Temp1 = aCap[2*i];
+      uint32_t u32Temp2 = aCap[2*i+1];
+      
+      if((aCap[2*i]>>15)==0)                      //check capacitance POS/NEG
       {                                          //if POS
         //Serial.println("Positive");            //delete "//" to debug
-        u32Cap = ((u32Cap1<<8)+(u32Cap2>>8));    //combine MSBs & LSBs
-        s32Cap = (int32_t)u32Cap;                //turn reading(unsign) into signed
+        uint32_t u32Cap = ((u32Temp1<<8)+(u32Temp2>>8));    //combine MSBs & LSBs
+        int32_t s32Cap = (int32_t)u32Cap;                //turn reading(unsign) into signed
         fCap = (double)s32Cap;                   //turn reading(signed) into float
         fCap/= 524288;                           ///2^19, see 8.6.1.1
       }
       else                                       //if NEG
       {
         //Serial.println("Negative");                  //delete "//" to debug
-        u32Cap = ((~u32Cap1+1)<<8+(~u32Cap2+1)>>8);    //turn MSBs & LSBs (2's COMP) into unsigned(POS), combine MSBs & LSBs
+        u32Cap = ((~u32Temp1+1)<<8+(~u32Temp2+1)>>8);    //turn MSBs & LSBs (2's COMP) into unsigned(POS), combine MSBs & LSBs
         s32Cap = ((int32_t)u32Cap)*(-1);               //turn reading(unsign) into signed, NEG
         fCap = (double)s32Cap;                         //turn reading(signed) into float
         fCap/= 524288;                                 ///2^19, see 8.6.1.1
--- T_09272015/T_09272015.ino
+++ T_09272015/T_09272015.ino
@@ -31,42 +31,41 @@
 /*-----( Declare Constants and Pin Numbers )-----*/
 #define CE_PIN   9
 #define CSN_PIN 10
-
+#define PIPE 0xE8E8F0F0E1LL    //Define the transmit pipe
+#define FDC B1010000    //FDC1004's address as a slave
 // NOTE: the "LL" at the end of the constant is "LongLong" type
-const uint64_t pipe = 0xE8E8F0F0E1LL; // Define the transmit pipe
-int FDC = B1010000;    //FDC1004's address as a slave
 
 /*-----( Declare objects )-----*/
-RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
+RF24 radio(CE_PIN, CSN_PIN);    //Create a Radio
 
 /*-----( Declare Variables )-----*/
-uint32_t aCap[2];
+uint16_t aCap[8];
+uint16_t u16Data;
  
-void setup()   /****** SETUP: RUNS ONCE ******/
+void setup()    /****** SETUP: RUNS ONCE ******/
 {
   Serial.begin(9600);
-  Wire.begin();                   //join I2C bus as a master
-  for(int i=0; i<4; i++)          //MEAS_CONF setup, CHB & CAPDAC disabled, see Table 4
+  Wire.begin();    //join I2C bus as a master
+  for(int i=0; i<4; i++)    //MEAS_CONF setup, CHB & CAPDAC disabled, see Table 4
   {
     FDC_write(0x08+i, 0x1c+0x20*i, 0x00);
   }
   FDC_write(0x0c, 0x05, 0xF0);    //FDC register setup, rate=100S/s, see Table 5
   
   radio.begin();
-  radio.openWritingPipe(pipe);  
+  radio.openWritingPipe(PIPE);  
 }//--(end setup )---
 
 
-void loop()   /****** LOOP: RUNS CONSTANTLY ******/
+void loop()    /****** LOOP: RUNS CONSTANTLY ******/
 {
-  for(int i=0; i<4; i++)
+  for(int i=0; i<8; i++)
   {
-    aCap[1] = FDC_read(0x00+2*i);
-    aCap[2] = FDC_read(0x01+2*i);
-    radio.write(aCap, sizeof(aCap) );
+    aCap[i] = FDC_read(0x00+i);
   }
+  radio.write(aCap, sizeof(aCap) );
   delay(1000);
-}//--(end main loop )---
+}    //--(end main loop )---
 
 /*-----( Declare User-written Functions )-----*/
   void FDC_write(uint8_t u8Poi, uint8_t u8Reg1, uint8_t u8Reg2)    //write FDC1004 (pointer, REG_MSB, REG_LSB)
@@ -84,9 +83,9 @@ uint16_t FDC_read(uint8_t u8Reg)    //read FDC1004 (REG)
   Wire.write(u8Reg);
   Wire.endTransmission();
   Wire.requestFrom(FDC,2);
-  uint8_t u8Temp1=Wire.read();              //MSB of returning data
-  uint8_t u8Temp2=Wire.read();              //LSB of returning data
-  uint16_t u16Data=(u8Temp1<<8)+u8Temp2;    //combine returning data
+  uint8_t u8Temp1 = Wire.read();    //MSB of returning data
+  uint8_t u8Temp2 = Wire.read();    //LSB of returning data
+  u16Data = (u8Temp1<<8)+u8Temp2;    //combine returning data
   return u16Data;
 }
   
